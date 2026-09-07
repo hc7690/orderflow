da0dc3df32829dabd11caccf31bd2cf87104c7aa
root <root@localhost.localdomain>
2026-09-07 10:56:50 +0000

    fix error login

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["firebase-admin"],
   /**
    * Declare packages that should only be used server-side.
    * This prevents them from being bundled into client code.
